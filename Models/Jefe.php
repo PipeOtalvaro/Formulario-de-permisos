8dff45275fcbba2abbd82f867dc9761f5729f9e2
Pipe Otálvaro <afotalvaroc@uqvirtual.edu.co>
2021-07-23 18:12:47 -0500

    Consultas para listar personas y jefes

--- Models/Jefe.php
+++ Models/Jefe.php
@@ -4,4 +4,16 @@ namespace Models;
 
 class Jefe extends Persona
 {
+
+    private $con;
+    public function __construct()
+    {
+        $this->con = new Conexion();
+    }
+    public function listar()
+    {
+        $sql = "SELECT CONCAT(j.nombre, ' ', j.primer_apellido, ' ', j.segundo_apellido ) nombre_completo FROM jefes j";
+        $datos = $this->con->consultaRetorno($sql);
+        return $datos;
+    }
 }
