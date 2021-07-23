90fc9a2678646386964622d572765df817b70035
Pipe Otálvaro <afotalvaroc@uqvirtual.edu.co>
2021-07-23 10:26:32 -0500

    Consulta que agrega un solicitante

--- Models/Solicitante.php
+++ Models/Solicitante.php
@@ -4,4 +4,10 @@ namespace Models;
 
 class Solicitante extends Persona
 {
+    public function add()
+    {
+        $sql = "INSERT INTO solicitantes (id, nombre, primer_apellido, segundo_apellido, telefono, email, create_at)
+        VALUES(null, '{$this->nombre}','{$this->primerApellido}','{$this->segundoApellido}','{$this->telefono}','{$this->email}', NOW()";
+        $this->con->consultaSimple($sql);
+    }
 }
