69ffd6eb3f3714a8074c8ae6c6881ee17abaf949
Pipe Otálvaro <afotalvaroc@uqvirtual.edu.co>
2021-07-24 18:02:35 -0500

    vistas del formulario

---
+++ Views/Includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilos.css" />
+    <meta charset="utf-8" />
+    <title>Formulario de solicitud</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
